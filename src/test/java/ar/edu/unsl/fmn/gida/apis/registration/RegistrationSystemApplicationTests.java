c73a45002b543fbdb3c63f4fcba74fbecf3081d5
dglabella <dglabella@gmail.com>
2022-08-17 15:08:57 -0300

    init commit

---
+++ src/test/java/ar/edu/unsl/fmn/gida/apis/registration/RegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unsl.fmn.gida.apis.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
