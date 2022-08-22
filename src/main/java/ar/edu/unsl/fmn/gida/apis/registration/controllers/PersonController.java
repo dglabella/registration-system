3bce67ccb3c3f522dc499687f037567c780a8442
AgenteBP <braianpaez1023@gmail.com>
2022-08-22 18:13:57 -0300

    22/08/2022 saving progress

--- src/main/java/ar/edu/unsl/fmn/gida/apis/registration/controllers/PersonController.java
+++ src/main/java/ar/edu/unsl/fmn/gida/apis/registration/controllers/PersonController.java
@@ -1,7 +1,12 @@
 package ar.edu.unsl.fmn.gida.apis.registration.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +25,16 @@ public class PersonController {
         System.out.println("YA ANDA ENTRO ACA WACHOOO");
         return null;
     }
+
+    @GetMapping
+    public ArrayList<Person> getAllPerson() {
+        return personService.getPerson();
+    }
+
+    @PostMapping
+    public Person postPerson(@RequestBody Person person) {
+        System.out.println("persona tiene " + person.getDni());
+        return personService.postPerson(person);
+    }
+
 }
