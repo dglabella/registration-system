7420fb4d65c7690d2a1aefab8d0f1c1ea9711a04
AgenteBP <braianpaez1023@gmail.com>
2022-11-12 13:28:25 -0300

    saving progress

--- src/main/java/ar/edu/unsl/fmn/gida/apis/registration/services/PersonService.java
+++ src/main/java/ar/edu/unsl/fmn/gida/apis/registration/services/PersonService.java
@@ -3,7 +3,6 @@ package ar.edu.unsl.fmn.gida.apis.registration.services;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
