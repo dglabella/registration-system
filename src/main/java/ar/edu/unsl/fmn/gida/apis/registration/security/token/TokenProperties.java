baf05133511e8de75c9bb45a63244005bd38c58e
dglabella <dglabella@gmail.com>
2023-08-07 17:33:33 -0300

    Update TokenProperties.java
    30 days token

--- src/main/java/ar/edu/unsl/fmn/gida/apis/registration/security/token/TokenProperties.java
+++ src/main/java/ar/edu/unsl/fmn/gida/apis/registration/security/token/TokenProperties.java
@@ -3,8 +3,9 @@ package ar.edu.unsl.fmn.gida.apis.registration.security.token;
 public interface TokenProperties {
 	String KEY = "(H+KbPeShVmYq3t6w9z$C&F)J@NcQfTj";
 
+	long EXPIRATION_TIME_SECONDS = 2592000; // 30 days
 	// long EXPIRATION_TIME_SECONDS = 86400; //24 hs
-	long EXPIRATION_TIME_SECONDS = 28800; // 8 hs
+	// long EXPIRATION_TIME_SECONDS = 28800; // 8 hs
 
 	String IDENTIFIER_KEY = "id";
 	String EMAIL_CLAIM_KEY = "email";
